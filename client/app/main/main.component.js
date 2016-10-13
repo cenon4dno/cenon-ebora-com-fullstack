041d1f6686b0852f7949aea6c6bd4e95c8a04b33
Cenon Ebora <remote.cenon@gmail.com>
2016-10-13 14:49:01 +0800

    initial code commit

---
+++ client/app/main/main.component.js
@@ -0,0 +1,48 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import routing from './main.routes';
+
+export class MainController {
+
+  /*@ngInject*/
+  constructor($http, $scope, socket) {
+    console.log('here 1');
+    this.$http = $http;
+    this.socket = socket;
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('thing');
+    });
+  }
+
+  $onInit() {
+    console.log('here 2');
+    this.$http.get('/api/things')
+      .then(response => {
+        console.log('response data', response.data);
+        this.awesomeThings = response.data;
+        this.socket.syncUpdates('thing', this.awesomeThings);
+      });
+  }
+
+  addThing() {
+    if(this.newThing) {
+      this.$http.post('/api/things', {
+        name: this.newThing
+      });
+      this.newThing = '';
+    }
+  }
+
+  deleteThing(thing) {
+    this.$http.delete('/api/things/' + thing._id);
+  }
+}
+
+export default angular.module('cenonEboraComFullstackApp.main', [uiRouter])
+  .config(routing)
+  .component('main', {
+    template: require('./main.html'),
+    controller: MainController
+  })
+  .name;
